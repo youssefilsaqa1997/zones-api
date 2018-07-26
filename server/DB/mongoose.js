45a7eb41d7f2e615f117eaa9cedd73bee7fd9f2e
youssef ilsaqa <youssef_ilsaqa@hotmail.com>
2018-07-26 16:56:28 +0200

    change DB from local to MLab

--- server/DB/mongoose.js
+++ server/DB/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose')
 
 mongoose.Promise =global.Promise;
-mongoose.connect('mongodb://localhost:27017/zonesApp')
+mongoose.connect('mongodb://<youssefilsaqa1997>:<Youssef.6>@ds253831.mlab.com:53831/zones')
 
 module.exports={mongoose};
